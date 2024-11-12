1c2311528c6232cce0db9839652d43f0557add20
ABINESH <94157292+Abi04nesh@users.noreply.github.com>
2024-11-12 10:07:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USER = "abi04nesh@gmail.com"
-EMAIL_PASSWORD = "rlcf egrn ppny xiwi"
+EMAIL_PASSWORD = ""
 
 HR_MANAGER_EMAIL = "abinesh.p.ihub@snsgroups.com"
